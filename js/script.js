b0cd765d9df116565c6f20d1c033b133ebf66c74
DuremarDuremar <duremar98@yandex,ru>
2020-04-09 17:56:14 +0300

    09.04.20(2)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("yy");
+$(document).ready(function () {
+  $(".slider").slick({
+    arrow: true,
+    dots: true, //точки
+    adaptiveHeight: true, //адаптив под высоту обьекта
+    slidesToShow: 1, //кол-во обьектов на странице
+    slidesToScroll: 1, //кол-во обьектов при перелистование
+    speed: 400, //скорость скролла
+    easing: "linear", //метод скролла
+    infinite: true, //конечность скролла
+    initialSlide: 0, //с какого начать
+    autoplay: false, //автопроигрывание
+    autoplaySpeed: 500,
+    centerMode: false,
+    variablewidth: false,
+    rows: 1,
+    slaiderPerRow: 1,
+    vertical: false,
+    fade: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        setting: {},
+      },
+    ],
+  });
+});
